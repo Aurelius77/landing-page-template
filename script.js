13639dac3ea34c639ae9783a612e427afdabed0f
Aurelius <89624318+Aurelius77@users.noreply.github.com>
2022-08-12 01:58:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function openMenu(){
+   document.querySelector('.modal').style.display = 'block'
+   document.querySelector('#menu').style.display = 'none'
+   document.querySelector('#close').style.display = 'block'
+
+}
+
+function closeMenu(){
+    document.querySelector('.modal').style.display = 'none'
+   document.querySelector('#menu').style.display = 'block'
+   document.querySelector('#close').style.display = 'none'
+}
